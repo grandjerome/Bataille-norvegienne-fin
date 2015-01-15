5c38470ae3dc310d83db9b37bc7263fe6186b46b
naooo <nao3@hotmail.fr>
2015-01-15 19:25:54 +0100

    Update Sept.java

--- src/moteur/Sept.java
+++ src/moteur/Sept.java
@@ -1,31 +1,46 @@
 package moteur;
 
-
-
+/**
+ * @author Naomi Sakine et Antoine Ladune
+ *  Classe Sept représentant les sept
+ */
 public class Sept extends moteur.CarteSpeciale {
+	/**
+	 * @param valeurcarte
+	 * @param couleurcarte
+	 * Constructeur de la classe Sept
+	 */
+	public Sept(int valeurcarte, String couleurcarte) {
+		super.couleur = couleurcarte;
+		super.valeur = valeurcarte;
 
-
-	
-
-	public Sept(int valeurcarte, String couleurcarte){
-		super.couleur=couleurcarte;
-		super.valeur=valeurcarte;
-		
 	};
-	
+
+	/* (non-Javadoc)
+	 * @see moteur.CarteSpeciale#jouerEffet()
+	 */
 	public void jouerEffet() {
 
 	}
+
+	/**
+	 * 
+	 */
 	public void donnerTalon() {
 
 	}
+
+	/**
+	 * 
+	 */
 	public void jouerCarteInferieure() {
 
 	}
-	
-	public String toString(){
-		//System.out.println("valeur en chiffre : "+ this.valeur);
-		return("Sept"+" de "+this.couleur);
-	}
 
+	/* (non-Javadoc)
+	 * @see moteur.Carte#toString()
+	 */
+	public String toString() {
+		return ("Sept" + " de " + this.couleur);
+	}
 }
