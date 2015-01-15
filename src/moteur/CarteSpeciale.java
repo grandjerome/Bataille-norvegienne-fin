31e96e1654d1efbe4bb70a011ec753bc3e191cd4
naooo <nao3@hotmail.fr>
2015-01-15 18:09:31 +0100

    Update CarteSpeciale.java

--- src/moteur/CarteSpeciale.java
+++ src/moteur/CarteSpeciale.java
@@ -1,27 +1,36 @@
 package moteur;
 
-
-
-
+/**
+ * @author Naomi Sakine et Antoine Ladune
+ *
+ */
 public abstract class CarteSpeciale extends moteur.Carte {
 	
-	
-	
+	/**
+	 * @param valeurcarte
+	 * @param couleurcarte
+	 * Constructeur de la classe CarteSpeciale
+	 */
 	public CarteSpeciale(int valeurcarte, String couleurcarte){
 		super.couleur=couleurcarte;
 		super.valeur=valeurcarte;
 		
 	}
+	/**
+	 * Constructeur de la classe CarteSpeciale qui ne prend aucun paramètre
+	 */
 	public CarteSpeciale(){
-		
 	}
 	
 	
+	/**
+	 * Méthode qui pose une carte spéciale
+	 */
 	public void poserCarteSpeciale() {
 
 	}
+	/**
+	 * Méthode abstraite qui joue l'effet d'une carte spéciale, redéfinie dans les classes filles
+	 */
 	public abstract void jouerEffet();
-	
-	
-
 }
