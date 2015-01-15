57998b875b093ece2f8be482b1200434ed36f6c4
naooo <nao3@hotmail.fr>
2015-01-15 18:04:39 +0100

    Update CarteInexistanteException.java

--- src/moteur/CarteInexistanteException.java
+++ src/moteur/CarteInexistanteException.java
@@ -1,5 +1,9 @@
 package moteur;
 
+/**
+ * @author Naomi Sakine et Antoine Ladune
+ *	Classe d'exception pour une carte inexistante
+ */
 public class CarteInexistanteException extends Exception{
 	public CarteInexistanteException() {
 		// TODO Auto-generated constructor stub
